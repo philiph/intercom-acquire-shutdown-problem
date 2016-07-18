f62bad22033172e6270ad49049973e0f2835fb5c
Philip Hagen <phil@philiphagen.com>
2016-07-18 20:22:41 +0800

    Proper sessions, remember is user signed in or not

--- server.rb
+++ server.rb
@@ -1,19 +1,52 @@
 require 'sinatra'
+require 'ostruct'
+require 'openssl'
 
-get '/' do
-  redirect '/signed_out'
-end
+enable :sessions
+
+set :intercom_app_id, 'xyz'
+set :intercom_secret, 'xyz'
 
 before do
-  @intercom_app_id = 'xyz'
+  @intercom_app_id = settings.intercom_app_id
+  load_user
 end
 
-get '/signed_out' do
-  @user_signed_in = false
-  erb :signed_out
+def load_user
+  user_id = session[:user_id]
+  if user_id == 9876
+    @user = OpenStruct.new(
+      user_id: '9876',
+      email: 'john.doe@example.com',
+      user_hash: user_hash(user_id),
+      created_at: 1234567890,
+      name: 'John Doe'
+    )
+  end
 end
 
-get '/signed_in' do
-  @user_signed_in = true
-  erb :signed_in
+def user_hash(user_id)
+  OpenSSL::HMAC.hexdigest(
+    OpenSSL::Digest.new('sha256'),
+    settings.intercom_secret,
+    user_id.to_s
+  )
+end
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/sign_in' do
+  session[:user_id] = 9876
+  redirect '/'
+end
+
+get '/sign_out' do
+  session[:user_id] = nil
+  redirect '/'
 end
